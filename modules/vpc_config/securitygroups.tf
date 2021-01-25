f47f9a0fc8cfdc22a00917b9cd1e213b30b01830
pavanmudduluru <m.pavan599@gmail.com>
2021-01-26 01:48:21 +0530

    Initial commint

---
+++ modules/vpc_config/securitygroups.tf
@@ -0,0 +1,127 @@
+resource "aws_security_group" "default" {
+  name        = "default"
+  description = "Default security group to allow inbound/outbound from the VPC"
+  vpc_id      = aws_vpc.vpc.id
+  depends_on  = [aws_vpc.vpc]
+  ingress {
+    from_port = "0"
+    to_port   = "0"
+    protocol  = "-1"
+    self      = true
+  } 
+  egress {
+    from_port = "0"
+    to_port   = "0"
+    protocol  = "-1"
+    self      = "true"
+  }
+  tags = {
+    Name = "default"
+  }
+}
+
+resource "aws_security_group" "nginx" {
+  name        = "nginx"
+  vpc_id = aws_vpc.vpc.id
+  depends_on  = [aws_vpc.vpc]
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "nginx"
+  }
+}
+
+resource "aws_security_group" "alb" {
+  name        = "alb"
+  vpc_id = aws_vpc.vpc.id
+  depends_on  = [aws_vpc.vpc]
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "alb"
+  }
+}
+
+resource "aws_security_group" "elb" {
+  name        = "elb"
+  vpc_id = aws_vpc.vpc.id
+  depends_on  = [aws_vpc.vpc]
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "elb"
+  }
+}
+
+resource "aws_security_group" "nodejs" {
+  name        = "nodejs"
+  vpc_id = aws_vpc.vpc.id
+  depends_on  = [aws_vpc.vpc]
+  ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "nodejs"
+  }
+}
+
+resource "aws_security_group" "ssh" {
+  name        = "ssh"
+  description = "Allow SSH inbound connections"
+  vpc_id = aws_vpc.vpc.id
+  depends_on  = [aws_vpc.vpc]
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "ssh"
+    cidr_blocks = ["10.0.0.0/16"]
+  }
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "ssh"
+  }
+}
---
+++ modules/vpc_config/variables.tf
@@ -0,0 +1,7 @@
+variable "aws_region" {
+  description = "eu-central-1"
+}
+
+variable "vpc_cidr" {
+  description = "The CIDR block of the vpc"
+}
